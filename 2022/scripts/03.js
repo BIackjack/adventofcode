2f6a298b55109cf0c03c1e206ab4dc3fb16a52b8
Thomas <thomasschrive@gmail.com>
2022-12-23 17:19:54 +0100

    Day 3 of 2022

---
+++ 2022/scripts/03.js
@@ -0,0 +1,44 @@
+const getLetterPriority = (letter) => {
+    const charCode = letter.charCodeAt(0);
+    if (97 <= charCode && charCode <= 122) {
+        // Lowercase
+        return charCode - 96;
+    } else {
+        // Uppercase
+        return charCode - 38;
+    }
+}
+
+const Utils = require('../../utils');
+Utils.getInput('03')
+.then(input => {
+    const lettersPart1 = input.map(line => {
+        const firstHalf = line.slice(0, line.length / 2);
+        const secondHalf = line.slice(line.length / 2);
+        return firstHalf.split('').find(char => secondHalf.includes(char));
+    });
+
+    const prioritiesPart1 = lettersPart1.map(getLetterPriority);
+    const sumPart1 = prioritiesPart1.reduce((acc, value) => acc + value, 0);
+
+    console.log(`Part1: ${sumPart1}`);
+
+    const lettersPart2 = [];
+    let currentGroup = 0;
+    
+    for (let i = 0; i < input.length / 3; i++) {
+        const firstLine = input[currentGroup * 3];
+        const secondLine = input[currentGroup * 3 + 1];
+        const thirdLine = input[currentGroup * 3 + 2];
+        
+        const letter = firstLine.split('').find(char => secondLine.includes(char) && thirdLine.includes(char));
+        lettersPart2.push(letter);
+
+        currentGroup++;
+    }
+
+    const prioritiesPart2 = lettersPart2.map(getLetterPriority);
+    const sumPart2 = prioritiesPart2.reduce((acc, value) => acc + value, 0);
+
+    console.log(`Part2: ${sumPart2}`);
+});
