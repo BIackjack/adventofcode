7bed1026e7e352db69e5e025f8cc24c82cc87399
Thomas <thomasschrive@gmail.com>
2022-12-24 09:49:44 +0100

    Day 4 of 2022

---
+++ 2022/scripts/04.js
@@ -0,0 +1,32 @@
+const getCurrentPairBounds = (currentPair) => {
+    return currentPair.split(',')
+                      .flatMap(a => a.split('-').map(b => +b));
+}
+
+const doThePairsEntirelyOverlap = (firstLowerBound, firstUpperBound, secondLowerBound, secondUpperBound) => {
+    const isFirstIncludedInTheSecond = firstLowerBound >= secondLowerBound && firstUpperBound <= secondUpperBound;
+    const isSecondIncludedInTheFirst = firstLowerBound <= secondLowerBound && firstUpperBound >= secondUpperBound;
+    return isFirstIncludedInTheSecond || isSecondIncludedInTheFirst;
+}
+
+const doThePairsPartiallyOverlap = (firstLowerBound, firstUpperBound, secondLowerBound, secondUpperBound) => {
+    return firstUpperBound >= secondLowerBound && secondUpperBound >= firstLowerBound;
+}
+
+const Utils = require('../../utils');
+Utils.getInput('04')
+.then(input => {
+    const totalTotallyOverlappingPairs = input.reduce((acc, currentPair) => {
+        const bounds = getCurrentPairBounds(currentPair);
+        return doThePairsEntirelyOverlap(...bounds) ? acc + 1 : acc;
+    }, 0);
+
+    console.log(`Part1: ${totalTotallyOverlappingPairs}`);
+
+    const totalPartiallyOverlappingPairs = input.reduce((acc, currentPair) => {
+        const bounds = getCurrentPairBounds(currentPair);
+        return doThePairsPartiallyOverlap(...bounds) ? acc + 1 : acc;
+    }, 0);
+
+    console.log(`Part2: ${totalPartiallyOverlappingPairs}`);
+});
