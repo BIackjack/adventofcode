0b34d36fdee715c8311ceaaa708c9dbe0a296200
Thomas <thomasschrive@gmail.com>
2023-01-14 14:18:31 +0100

    Day 2 par 2 complete

--- src/app/views/2022/day-02/day-02.component.ts
+++ src/app/views/2022/day-02/day-02.component.ts
@@ -41,27 +41,31 @@ const getMatchOutcomePointsPart1 = (char1: FirstChar, char2: SecondChar): BoutOu
     }
 }
 
-// const getMatchOutcomePointsPart2 = (char1, char2) => {
-//     if (char1 === 'A' && char2 === 'X') {
-//         return 0 + 3;
-//     } else if (char1 === 'A' && char2 === 'Y') {
-//         return 3 + 1;
-//     } else if (char1 === 'A' && char2 === 'Z') {
-//         return 6 + 2;
-//     } else if (char1 === 'B' && char2 === 'X') {
-//         return 0 + 1;
-//     } else if (char1 === 'B' && char2 === 'Y') {
-//         return 3 + 2;
-//     } else if (char1 === 'B' && char2 === 'Z') {
-//         return 6 + 3;
-//     } else if (char1 === 'C' && char2 === 'X') {
-//         return 0 + 2;
-//     } else if (char1 === 'C' && char2 === 'Y') {
-//         return 3 + 3;
-//     } else if (char1 === 'C' && char2 === 'Z') {
-//         return 6 + 1;
-//     }
-// }
+const getMatchOutcomePointsPart2 = (char1: FirstChar, char2: SecondChar): BoutOutcome => {
+    const opponentMove: Move = char1 === 'A' ? 'rock' : char1 === 'B' ? 'paper' : 'scissors';
+    const nbPointsFromResult: number = char2 === 'X' ? 0 : char2 === 'Y' ? 3 : 6;
+    const result: Result = char2 === 'X' ? 'loss' : char2 === 'Y' ? 'draw' : 'win';
+
+    if (char1 === 'A' && char2 === 'X') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 3, myMove: 'scissors'};
+    } else if (char1 === 'A' && char2 === 'Y') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 1, myMove: 'rock'};
+    } else if (char1 === 'A' && char2 === 'Z') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 2, myMove: 'paper'};
+    } else if (char1 === 'B' && char2 === 'X') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 1, myMove: 'rock'};
+    } else if (char1 === 'B' && char2 === 'Y') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 2, myMove: 'paper'};
+    } else if (char1 === 'B' && char2 === 'Z') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 3, myMove: 'scissors'};
+    } else if (char1 === 'C' && char2 === 'X') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 2, myMove: 'paper'};
+    } else if (char1 === 'C' && char2 === 'Y') {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 3, myMove: 'scissors'};
+    } else /* if (char1 === 'C' && char2 === 'Z') */ {
+        return {result, opponentMove, nbPointsFromResult, nbPointsFromMyMove: 1, myMove: 'rock'};
+    }
+}
 
 @Component({
   selector: 'year-2022-day-02',
@@ -70,35 +74,39 @@ const getMatchOutcomePointsPart1 = (char1: FirstChar, char2: SecondChar): BoutOu
 })
 export class PuzzleYear2022Day02Component implements OnInit {
     chart1!: echarts.ECharts;
+    chart2!: echarts.ECharts;
 
     part1Answer = 0;
     part2Answer = 0;
-    part2Explanation = '';
 
     ngOnInit(): void {
         this.chart1 = echarts.init(document.getElementById('chart1') as HTMLElement, 'dark');
+        this.chart2 = echarts.init(document.getElementById('chart2') as HTMLElement, 'dark');
     }
 
     onInputRun(rawInput: string) {
         const input = rawInput.replaceAll('\r\n', '\n');
-        const bouts = input.split('\n').map((bout, i) => ({name: `Bout #${i + 1}`, data: getMatchOutcomePointsPart1(bout[0] as FirstChar, bout[2] as SecondChar)}));
 
-        const data = bouts.map(e => e.name);
+        // Part 1
 
-        const sumData = bouts.reduce((acc: number[], bout) => {
+        const boutsPart1 = input.split('\n').map((bout, i) => ({name: `Bout #${i + 1}`, data: getMatchOutcomePointsPart1(bout[0] as FirstChar, bout[2] as SecondChar)}));
+
+        const data = boutsPart1.map(e => e.name);
+
+        const sumDataPart1 = boutsPart1.reduce((acc: number[], bout) => {
             const currentBoutPoints = bout.data.nbPointsFromResult + bout.data.nbPointsFromMyMove;
             const boutsSum = acc[acc.length - 1] ?? 0;
             return [...acc, boutsSum + currentBoutPoints];
         }, []);
 
-        const dataset: DatasetOption = {
+        const datasetPart1: DatasetOption = {
             source: [
                 ['Bout', 'My move', 'Nb of points from my move', 'Opponent\'s move', 'Bout result', 'Nb of points from result'],
-                ...bouts.map(({data, name}) => [name, data.myMove, data.nbPointsFromMyMove, data.opponentMove, data.result, data.nbPointsFromResult]),
+                ...boutsPart1.map(({data, name}) => [name, data.myMove, data.nbPointsFromMyMove, data.opponentMove, data.result, data.nbPointsFromResult]),
             ]
         }
 
-        const series: Array<LineSeriesOption | BarSeriesOption> = [
+        const seriesPart1: Array<LineSeriesOption | BarSeriesOption> = [
             {
                 name: 'Points from my move',
                 type: 'bar',
@@ -131,7 +139,7 @@ export class PuzzleYear2022Day02Component implements OnInit {
                     color: '#EE6666',
                 },
                 yAxisIndex: 1,
-                data: sumData,
+                data: sumDataPart1,
                 markPoint: {
                     symbol: 'roundRect',
                     emphasis: {
@@ -176,7 +184,6 @@ export class PuzzleYear2022Day02Component implements OnInit {
                     tooltip += nbPointsFromMyMoveTooltip;
                     tooltip += '<hr>';
                     tooltip += sumTooltip;
-                    console.log(params, tooltip);
                     return tooltip;
                 }
             },
@@ -201,13 +208,135 @@ export class PuzzleYear2022Day02Component implements OnInit {
                     name: 'Point sum',
                 },
             ],
-            dataset,
-            series,
+            dataset: datasetPart1,
+            series: seriesPart1,
         });
 
-        this.part1Answer = sumData[sumData.length - 1];
-        // const last3Elves = elves.slice(-3);
-        // this.part2Answer = sum(...last3Elves.flatMap(e => e.data));
-        // this.part2Explanation = last3Elves.map(e => sum(...e.data)).join(' + ');
+        this.part1Answer = sumDataPart1[sumDataPart1.length - 1];
+
+        // Part 2
+
+        const boutsPart2 = input.split('\n').map((bout, i) => ({name: `Bout #${i + 1}`, data: getMatchOutcomePointsPart2(bout[0] as FirstChar, bout[2] as SecondChar)}));
+
+        const sumDataPart2 = boutsPart2.reduce((acc: number[], bout) => {
+            const currentBoutPoints = bout.data.nbPointsFromResult + bout.data.nbPointsFromMyMove;
+            const boutsSum = acc[acc.length - 1] ?? 0;
+            return [...acc, boutsSum + currentBoutPoints];
+        }, []);
+
+        const datasetPart2: DatasetOption = {
+            source: [
+                ['Bout', 'My move', 'Nb of points from my move', 'Opponent\'s move', 'Bout result', 'Nb of points from result'],
+                ...boutsPart2.map(({data, name}) => [name, data.myMove, data.nbPointsFromMyMove, data.opponentMove, data.result, data.nbPointsFromResult]),
+            ]
+        }
+
+        const seriesPart2: Array<LineSeriesOption | BarSeriesOption> = [
+            {
+                name: 'Points from my move',
+                type: 'bar',
+                stack: 'Total',
+                yAxisIndex: 0,
+                encode: {
+                    x: 'Bout',
+                    y: 'Nb of points from my move',
+                },
+            },
+            {
+                name: 'Points from bout result',
+                type: 'bar',
+                stack: 'Total',
+                yAxisIndex: 0,
+                encode: {
+                    x: 'Bout',
+                    y: 'Nb of points from result',
+                },
+            },
+            {
+                name: 'Point sum',
+                type: 'line',
+                smooth: 0.5,
+                lineStyle: {
+                    color: '#EE6666',
+                    width: 4,
+                },
+                itemStyle: {
+                    color: '#EE6666',
+                },
+                yAxisIndex: 1,
+                data: sumDataPart2,
+                markPoint: {
+                    symbol: 'roundRect',
+                    emphasis: {
+                        disabled: true,
+                    },
+                    data: [
+                        { type: 'max', name: 'Max' },
+                    ]
+                },
+            },
+        ];
+
+        this.chart2.clear();
+        this.chart2.setOption({
+            tooltip: {
+                trigger: 'axis',
+                order: 'seriesDesc',
+                axisPointer: {
+                    type: 'shadow',
+                },
+                formatter: function(params: CallbackDataParams[]) {
+                    const sumData = params.pop();
+                    const sumTooltip = `<div>${sumData?.marker} Current point sum: <strong>${sumData?.data}</strong></div>`;
+
+                    const [, myMove, nbPointsFromMyMove, opponentMove, boutResult, nbPointsFromResult] = params[0].value as unknown[];
+
+                    const nbPointsFromMyMoveTooltip = `<div>${params[0].marker} ${params[0].seriesName}: <strong>${nbPointsFromMyMove ?? '-'}</strong></div>`;
+                    const nbPointsFromResultTooltip = `<div>${params[1].marker} ${params[1].seriesName}: <strong>${nbPointsFromResult ?? '-'}</strong></div>`;
+
+                    const opoonentMoveTooltip = `<img src="assets/icons/${opponentMove}.svg" width="${boutResult === 'loss' ? '40px' : 'auto'}"/>`;
+                    const myMoveTooltip = `<img src="assets/icons/${myMove}.svg" width="${boutResult === 'win' ? '40px' : 'auto'}"/>`;
+                    const boutTooltip = `
+                    <table>
+                        <tr><td>         Me         </td><td>          </td><td>         Opponent         </td></tr>
+                        <tr><td>  ${myMoveTooltip}  </td><td>  versus  </td><td>  ${opoonentMoveTooltip}  </td></tr>
+                    </table>
+                    `;
+
+                    let tooltip = `<div class="tooltip-label">${params[0].name}</div>`;
+                    tooltip += boutTooltip;
+                    tooltip += nbPointsFromResultTooltip;
+                    tooltip += nbPointsFromMyMoveTooltip;
+                    tooltip += '<hr>';
+                    tooltip += sumTooltip;
+                    return tooltip;
+                }
+            },
+            legend: {
+                top: 20,
+            },
+            grid: {
+                top: 100,
+            },
+            xAxis: {
+                type: 'category',
+                data,
+            },
+            yAxis: [
+                {
+                    type: 'value',
+                    name: 'Bout points',
+                    show: false,
+                },
+                {
+                    type: 'value',
+                    name: 'Point sum',
+                },
+            ],
+            dataset: datasetPart2,
+            series: seriesPart2,
+        });
+
+        this.part2Answer = sumDataPart2[sumDataPart2.length - 1];
     }
 }
