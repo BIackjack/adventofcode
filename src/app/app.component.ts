9182ff332ddb8d17bb8ecdad6cc6e6b1750d8de6
Thomas <thomasschrive@gmail.com>
2023-02-19 18:31:11 +0100

    Day 9 part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,7 +77,7 @@ export class AppComponent {
             },
             {
                 title: 'Day 9',
-                icon: 'more-horizontal-outline',
+                icon: 'checkmark-circle-2-outline',
                 link: '/2022/09',
                 pathMatch: 'full',
             },
--- src/app/views/2022/day-09/day-09.component.ts
+++ src/app/views/2022/day-09/day-09.component.ts
@@ -12,6 +12,12 @@ type Part1BoardState = {
     currentHeadPosition: Coordinates;
 }
 
+type Part2BoardState = {
+    tailPositionHistory: TailPositionHistory;
+    currentHeadPosition: Coordinates;
+    currentTailsPosition: Coordinates[];
+}
+
 const moveHead = (currentHeadPosition: Coordinates, direction: Direction): Coordinates => {
     const [currentX, currentY] = currentHeadPosition;
     switch (direction) {
@@ -44,6 +50,54 @@ const moveTail = (currentTailPosition: Coordinates, targetPosition: Coordinates)
     }
 }
 
+const getChartOptions = (dataset: [number, number, string][], minX: number, maxX: number, maxValue: number) => ({
+    grid: {
+        height: '70%',
+        top: '10%'
+    },
+    xAxis: {
+        type: 'category',
+        splitArea: {
+            show: true
+        }
+    },
+    yAxis: {
+        type: 'category',
+        splitArea: {
+            show: true
+        }
+    },
+    visualMap: {
+        min: 1,
+        max: maxValue,
+        calculable: true,
+        orient: 'horizontal',
+        left: 'center',
+        bottom: '5%',
+        text: ['Highest', 'Lowest'],
+    },
+    title: {
+        text: 'Number of times the tail visited each cell',
+        left: 'center',
+        top: 20,
+    },
+    series: [
+        {
+            type: 'heatmap',
+            data: dataset,
+            label: {
+                show: Math.abs(maxX - minX) < 20,
+            },
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 15,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+})
+
 @Component({
   selector: 'year-2022-day-09',
   templateUrl: './day-09.component.html',
@@ -53,9 +107,10 @@ export class PuzzleYear2022Day09Component implements OnInit {
     chart1!: echarts.ECharts;
     chart2!: echarts.ECharts;
 
+    part1BoardState!: Part1BoardState;
     part1Answer = 0;
     
-    part1BoardState!: Part1BoardState;
+    part2BoardState!: Part2BoardState;
     part2Answer = 0;
 
     ngOnInit(): void {
@@ -89,13 +144,11 @@ export class PuzzleYear2022Day09Component implements OnInit {
                 const [tailPositionX, tailPositionY] = this.part1BoardState.currentTailPosition;
                 const tailPositionHistory: number | undefined = this.part1BoardState.tailPositionHistory[`${tailPositionX}~${tailPositionY}`];
                 this.part1BoardState.tailPositionHistory[`${tailPositionX}~${tailPositionY}`] = (tailPositionHistory ?? 0) + (hasTailMoved ? 1 : 0);
-
-                console.log('iteration', direction, nbOfCells - i, hasTailMoved, structuredClone(this.part1BoardState));
             }
         });
 
         // Find the min/max points in each dimension in order to plot the chart
-        const [minX, maxX, minY, maxY, maxValue] = Object.keys(this.part1BoardState.tailPositionHistory).reduce(([minX, maxX, minY, maxY, maxValue], currentKey) => {
+        let [minX, maxX, minY, maxY, maxValue] = Object.keys(this.part1BoardState.tailPositionHistory).reduce(([minX, maxX, minY, maxY, maxValue], currentKey) => {
             const [currentX, currentY] = currentKey.split('~');
             
             return [
@@ -116,62 +169,68 @@ export class PuzzleYear2022Day09Component implements OnInit {
         }
         
         this.chart1.clear();
-        this.chart1.setOption({
-            grid: {
-                height: '70%',
-                top: '10%'
-            },
-            xAxis: {
-                type: 'category',
-                splitArea: {
-                    show: true
-                }
-            },
-            yAxis: {
-                type: 'category',
-                splitArea: {
-                    show: true
-                }
-            },
-            visualMap: {
-                min: 1,
-                max: maxValue,
-                calculable: true,
-                orient: 'horizontal',
-                left: 'center',
-                bottom: '5%',
-                text: ['Highest', 'Lowest'],
-            },
-            title: {
-                text: 'Number of times the tail visited each cell',
-                left: 'center',
-                top: 20,
-            },
-            series: [
-                {
-                    type: 'heatmap',
-                    data: chart1Dataset,
-                    label: {
-                        show: Math.abs(maxX - minX) < 20,
-                    },
-                    emphasis: {
-                        itemStyle: {
-                            shadowBlur: 15,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
-                    }
-                }
-            ]
-        });
+        this.chart1.setOption(getChartOptions(chart1Dataset, minX, maxX, maxValue));
 
         this.part1Answer = Object.keys(this.part1BoardState.tailPositionHistory).length;
 
         // Part 2
 
-        this.chart2.clear();
-        this.chart2.setOption({
+        this.part2BoardState = {
+            currentHeadPosition: [0, 0],
+            currentTailsPosition: Array(9).fill([0, 0]),
+            tailPositionHistory: {'0~0': 1},
+        };
+
+        instructions.forEach(([direction, nbOfCells]) => {
+            for (let i = 0; i < nbOfCells; i++) {
+                // Move the head
+                this.part2BoardState.currentHeadPosition = moveHead(this.part2BoardState.currentHeadPosition, direction);
+                
+                // Adjust the tails according to the head
+                for (let tailIndex = 0; tailIndex < 9; tailIndex++) {
+                    const tailCoordinates = this.part2BoardState.currentTailsPosition[tailIndex];
+                    const targetCoordinates = tailIndex === 0 ? this.part2BoardState.currentHeadPosition : this.part2BoardState.currentTailsPosition[tailIndex - 1];
+                    const newTailCoordinates = moveTail(tailCoordinates, targetCoordinates);
+                    // Save new tail coordinates
+                    this.part2BoardState.currentTailsPosition[tailIndex] = newTailCoordinates; 
+
+                    // Handling the last tail
+                    if (tailIndex === 8) {
+                        const hasTailMoved = tailCoordinates !== newTailCoordinates;
+                        
+                        // Increment the count for the current tail position
+                        const [tailPositionX, tailPositionY] = newTailCoordinates;
+                        const tailPositionHistory: number | undefined = this.part2BoardState.tailPositionHistory[`${tailPositionX}~${tailPositionY}`];
+                        this.part2BoardState.tailPositionHistory[`${tailPositionX}~${tailPositionY}`] = (tailPositionHistory ?? 0) + (hasTailMoved ? 1 : 0);
+                    }
+                }
+            }
         });
 
-        // this.part2Answer = scenicScoreMax;
+        // Find the min/max points in each dimension in order to plot the chart
+        [minX, maxX, minY, maxY, maxValue] = Object.keys(this.part2BoardState.tailPositionHistory).reduce(([minX, maxX, minY, maxY, maxValue], currentKey) => {
+            const [currentX, currentY] = currentKey.split('~');
+            
+            return [
+                Math.min(parseInt(currentX), minX),
+                Math.max(parseInt(currentX), maxX),
+                Math.min(parseInt(currentY), minY),
+                Math.max(parseInt(currentY), maxY),
+                Math.max(this.part2BoardState.tailPositionHistory[currentKey as keyof TailPositionHistory], maxValue),
+            ]
+        }, [0, 0, 0, 0, 0]);
+
+        // Build the dataset
+        const chart2Dataset: [number, number, string][] = [];
+        for (let x = minX; x <= maxX; x++) {
+            for (let y = minY; y <= maxY; y++) {
+                chart2Dataset.push([x, y, this.part2BoardState.tailPositionHistory[`${x}~${y}`]?.toString() ?? '-']);
+            }
+        }
+
+        this.chart2.clear();
+        this.chart2.setOption(getChartOptions(chart2Dataset, minX, maxX, maxValue));
+
+        this.part2Answer = Object.keys(this.part2BoardState.tailPositionHistory).length;;
     }
 }
