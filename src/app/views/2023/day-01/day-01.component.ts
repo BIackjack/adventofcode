f4d8e4135b2c01b56168be81afeaed85f7c1243d
Thomas <thomasschrive@gmail.com>
2023-12-01 23:05:07 +0100

    refactor 2023 Day 1 to have correct letters

--- src/app/views/2023/day-01/day-01.component.ts
+++ src/app/views/2023/day-01/day-01.component.ts
@@ -13,23 +13,27 @@ type Data = Array<{
     combinedDigit: number;
 }>
 
-type Part2Data = Array<{
-    prefix: string;
-    firstDigitChars: string;
-    middle: string;
-    secondDigitChars: string;
-    suffix: string;
-    combinedDigit: number;
-}>
-
-const getLetterPriority = (letter: string): number => {
-    const charCode = letter.charCodeAt(0);
-    if (97 <= charCode && charCode <= 122) {
-        // Lowercase
-        return charCode - 96;
-    } else {
-        // Uppercase
-        return charCode - 38;
+const digitToLetter = (digit: string): string => {
+    switch (digit) {
+        case '1':
+            return 'one';
+        case '2':
+            return 'two';         
+        case '3':
+            return 'three';            
+        case '4':
+            return 'four';            
+        case '5':
+            return 'five';
+        case '6':
+            return 'six';
+        case '7':
+            return 'seven';
+        case '8':
+            return 'eight';
+        case '9':
+        default:
+            return 'nine';
     }
 }
 
@@ -40,11 +44,11 @@ const getLetterPriority = (letter: string): number => {
 })
 export class PuzzleYear2023Day01Component {
     part1Data: Data | undefined;
-    part1Answer = 0;
+    part1Answer: number | string = 0;
     part1Explanation = '';
     
     part2Data: Data | undefined;
-    part2Answer = 0;
+    part2Answer: number | string  = 0;
     part2Explanation = '';
 
     onInputRun(rawInput: string) {
@@ -69,36 +73,50 @@ export class PuzzleYear2023Day01Component {
             };
         });
 
-        this.part1Answer = sum(...this.part1Data.map(d => d.combinedDigit));
-        this.part1Explanation = this.part1Data.map(d => d.combinedDigit).join(' + ');
+        const part1ComputedSum = sum(...this.part1Data.map(d => d.combinedDigit));
+        this.part1Answer = isNaN(part1ComputedSum) ? 'This part does not yield a solution' : part1ComputedSum;
+        this.part1Explanation = isNaN(part1ComputedSum) ? '' : this.part1Data.map(d => d.combinedDigit).join(' + ');
 
         this.part2Data = lines.map(line => {
             const replaced = line
-                .replace(/oneight/g, '18')
-                .replace(/twone/g, '21')
-                .replace(/threeight/g, '38')
-                .replace(/fiveight/g, '58')
-                .replace(/sevenine/g, '79')
-                .replace(/eightwo/g, '82')
-                .replace(/eighthree/g, '83')
-                .replace(/nineight/g, '98')
-                .replace(/one/g, '1')
-                .replace(/two/g, '2')
-                .replace(/three/g, '3')
-                .replace(/four/g, '4')
-                .replace(/five/g, '5')
-                .replace(/six/g, '6')
-                .replace(/seven/g, '7')
-                .replace(/eight/g, '8')
-                .replace(/nine/g, '9')
+                .replace(/oneight/g, '#1#8#')
+                .replace(/twone/g, '#2#1#')
+                .replace(/threeight/g, '#3#8#')
+                .replace(/fiveight/g, '#5#8#')
+                .replace(/sevenine/g, '#7#9#')
+                .replace(/eightwo/g, '#8#2#')
+                .replace(/eighthree/g, '#8#3#')
+                .replace(/nineight/g, '#9#8#')
+                .replace(/one/g, '#1#')
+                .replace(/two/g, '#2#')
+                .replace(/three/g, '#3#')
+                .replace(/four/g, '#4#')
+                .replace(/five/g, '#5#')
+                .replace(/six/g, '#6#')
+                .replace(/seven/g, '#7#')
+                .replace(/eight/g, '#8#')
+                .replace(/nine/g, '#9#')
             const charArray = replaced.split('')
-            const firstDigitIndex = charArray.findIndex(c => c.match(/\d/));
-            const lastDigitIndex = charArray.length - 1 - [...charArray].reverse().findIndex(c => c.match(/\d/));
-            
+            const firstDigitIndex = charArray.findIndex(c => c.match(/(#\d#)|\d/));
+            const lastDigitIndex = charArray.length - 1 - [...charArray].reverse().findIndex(c => c.match(/(#\d#)|\d/));
+            const isFirstDigitInLetters = replaced[firstDigitIndex - 1] === '#';
+            const isLastDigitInLetters = replaced[lastDigitIndex + 1] === '#';
+
             const chars = charArray.map((char, index): CharData => {
-                const isHighlighted = index === firstDigitIndex || index === lastDigitIndex;
-                return {char, isHighlighted};
-            });
+                if (index === firstDigitIndex) {
+                    return {
+                        char: isFirstDigitInLetters ? digitToLetter(char) : char,
+                        isHighlighted: true,
+                    };
+                } else if (index === lastDigitIndex) {
+                    return {
+                        char: isLastDigitInLetters ? digitToLetter(char) : char,
+                        isHighlighted: true,
+                    };
+                } else {
+                    return {char, isHighlighted: false};
+                }
+            }).filter(({char}) => char !== '#');
             
             return {
                 chars,
@@ -107,46 +125,9 @@ export class PuzzleYear2023Day01Component {
                 combinedDigit: parseInt(`${charArray[firstDigitIndex]}${charArray[lastDigitIndex]}`),
             };
         });
-    
-        // this.part2Data = lines.map(line => {
-        //     const wordMatches = Array.from(line.matchAll(/(?=(one|two|three|four|five|six|seven|eight|nine))/g));
-        //     const digitMatches = Array.from(line.matchAll(/\d/g));
-        //     const minWordIndex = Math.min(...wordMatches.map((m) => m.index ?? Infinity));
-        //     const minDigitIndex = Math.min(...digitMatches.map((m) => m.index ?? Infinity));
-        //     const maxWordIndex = Math.max(...wordMatches.map((m) => m.index ?? -Infinity));
-        //     const maxDigitIndex = Math.max(...digitMatches.map((m) => m.index ?? -Infinity));
 
-        //     if (minWordIndex < minDigitIndex && maxWordIndex < maxDigitIndex) {
-        //         return {
-        //             prefix: line.slice(0, minWordIndex),
-        //             firstDigitChars
-        //         }
-        //     }
-
-        //     return {
-        //         prefix: line.slice(0, minWordIndex),
-        //         firstDigitChars: wordMatches.find((m) => m.index === minWordIndex)?.[1],
-        //         middle: line.slice(minWordIndex + 1)
-        //     }
-
-            // const charArray = line.split('')
-            // const firstDigitIndex = charArray.findIndex(c => c.match(/\d/));
-            // const lastDigitIndex = charArray.length - 1 - [...charArray].reverse().findIndex(c => c.match(/\d/));
-            
-            // const chars = charArray.map((char, index): CharData => {
-            //     const isHighlighted = index === firstDigitIndex || index === lastDigitIndex;
-            //     return {char, isHighlighted};
-            // });
-            
-            // return {
-            //     chars,
-            //     firstDigit: charArray[firstDigitIndex],
-            //     secondDigit: charArray[lastDigitIndex],
-            //     combinedDigit: parseInt(`${charArray[firstDigitIndex]}${charArray[lastDigitIndex]}`),
-            // };
-        // });
-
-        this.part2Answer = sum(...this.part2Data.map(d => d.combinedDigit));
-        this.part2Explanation = this.part2Data.map(d => d.combinedDigit).join(' + ');
+        const part2ComputedSum = sum(...this.part2Data.map(d => d.combinedDigit));
+        this.part2Answer = isNaN(part2ComputedSum) ? 'This part does not yield a solution' : part2ComputedSum;
+        this.part2Explanation = isNaN(part2ComputedSum) ? '' : this.part2Data.map(d => d.combinedDigit).join(' + ');
     }
 }
